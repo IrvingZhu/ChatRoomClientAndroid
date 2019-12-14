220f51357a9b0f599a79595e8aa96419c85cc918
zrz <316677100@qq.com>
2019-12-14 23:09:39 +0800

    AddLoginInterface

--- app/src/main/java/com/example/chatroomclient/MainActivity.java
+++ app/src/main/java/com/example/chatroomclient/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.chatroomclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.os.Bundle;
 
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Login = findViewById(R.id.Login);
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Socket sock = new Socket();
+            }
+        });
     }
 }
